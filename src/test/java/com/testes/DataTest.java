9a6b747ea5647c1192fb718dc8893b8e0e4b1f25
pedro-ddias <pedro.hdias@outlook.com>
2022-10-16 18:31:40 -0300

    Comentários em DataTest.java

--- src/test/java/com/testes/DataTest.java
+++ src/test/java/com/testes/DataTest.java
@@ -27,6 +27,7 @@ public class DataTest {
     }
 
     @Test
+    // Verifica se a soma de dias está correta, comparando com uma data, de acordo com uma data passada e solicita a somar dias
     public void test_somarDias(){
         Data dataSomada = new Data(9,10,2020);
         Data auxData = this.dataAtual.somarDias(5);
@@ -34,26 +35,31 @@ public class DataTest {
     }
 
     @Test
+    // Verifica se o método esta retornando o mes correto
     public void test_mes(){
         assertTrue(this.dataAtual.mesesEsseAno(this.dataAtual) == this.mes);
     }
 
       @Test
+      // Verifica se o método está retornando o ano correto
     public void test_ano(){
         assertTrue(this.dataAtual.anosEsseAno(this.dataAtual) == this.anoBi);
     }
 
     @Test
+    // Verifica se o método está retornando o dia correto
     public void test_dia(){
         assertTrue(this.dataAtual.diasEsseAno(this.dataAtual) == this.dia);
     }
 
     @Test
+    // Vericia se a data é mais recente, caso for retorna 1
     public void test_maisRecente(){
         assertTrue(this.dataAtual.maisRecente(this.data) == 1);
     }
 
     @Test
+    // Verifica se o método diferença de mês retorna a quantidade correta em relação a data já predefinida no sistema
     public void test_diferencaMeses(){
         assertTrue(this.data.diferencaMeses(this.dataAtual) == 1449);
     }
