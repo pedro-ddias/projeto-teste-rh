70e9d3bda41da88d4cd2e841e01124ddf334202b
Octavio Martins <octaviomartins10@gmail.com>
2022-10-16 16:12:26 -0300

    Subindo alterações Empresa teste João

--- src/test/java/com/testes/EmpresaTest.java
+++ src/test/java/com/testes/EmpresaTest.java
@@ -1,5 +1,6 @@
 package com.testes;
 import com.testes.Funcionario;
+import com.testes.Data;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertTrue;
@@ -9,6 +10,8 @@ import org.junit.Test;
 public class EmpresaTest {
 
     Empresa empresa = new Empresa();
+    Data data = new Data();
+    Data dataHoje = new Data(data.retornaDiaAtual(), data.retornaMesAtual(), data.retornaAnoAtual());
     Gerente gerente1 = new Gerente("Flavio Augusto", "12345678901234");
     Gerente gerente2 = new Gerente("Pedro Octavio", "12345678905432");
     Diretor diretor1 = new Diretor("Roberto Fernandes", "09876543211234");
@@ -31,13 +34,25 @@ public class EmpresaTest {
     }
 
     @Test
-    public void test_calculaFolhaGerente(){
-        gerente1.setSalario_base(3000);
-        gerente2.setSalario_base(3000);
-        empresa.adicionaFuncionario(0, gerente1);
-        empresa.adicionaFuncionario(0, gerente2);
-        
-
+    public void test_calculaFolhaGerenteEDiretor(){
+        this.gerente1.setSalario_base(3000);
+        this.gerente1.setAdmissao(this.dataHoje);
+        this.gerente2.setSalario_base(3000);
+        this.gerente2.setAdmissao(this.dataHoje);
+        this.empresa.adicionaFuncionario(0, gerente1);
+        this.empresa.adicionaFuncionario(0, gerente2);
+        assertTrue(this.empresa.calculaFolhaGerente() == 6000);
     }
 
+    @Test
+    public void test_calculaFolhaAnalista(){
+        this.junior1.setSalario(1_040); // salario real deve ser R$962,00
+        this.junior1.setMesesTrab(1);
+        this.senior1.setSalario(3_134); // salario real deve ser R$2,757.92
+        this.senior1.setMesesTrab(1);
+        this.empresa.adicionaFuncionario(2, junior1);
+        this.empresa.adicionaFuncionario(2, senior1);
+        assertTrue(this.empresa.calculaFolhaAnalista() == 3_719.92);
+
+    }
 }
