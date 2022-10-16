e035c948a1623bc9105f3009d6d86b0313a2217b
pedro-ddias <pedro.hdias@outlook.com>
2022-10-16 00:03:33 -0300

    Teste Analista e Diretor

--- src/test/java/com/testes/DiretorTest.java
+++ src/test/java/com/testes/DiretorTest.java
@@ -1,9 +1,43 @@
 package com.testes;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
 
 import org.junit.Test;
 
 public class DiretorTest {
     
+    Diretor diretor = new Diretor("Daniel Trindade", "52044686515");
+    Diretor diretor2 = new Diretor("Maycon Felipe", "42574856620");
+
+    @Test //se objeto não está retornando nulo
+    public void test_Diretor() {
+        assertNotNull(diretor);
+    }
+
+    @Test //se objeto não é o mesmo
+    public void test_Diretor2() {
+        assertNotSame(diretor, diretor2);
+    }
+
+    @Test
+    public void test_icalculaBonus() {
+        //passar o salario base
+        diretor.setSalario_base(10_000);
+        //passar data de admissao
+        diretor.setAdmissao(new Data(17,3,2018));
+        assertEquals(4_000, diretor.icalculaBonus(), 000);
+    }
+
+    @Test
+    public void test_calculaSalario() {
+        //passar o salario base
+        diretor.setSalario_base(10_000);
+        //passar data de admissao
+        diretor.setAdmissao(new Data(17,3,2018));
+        assertEquals(14_000, diretor.calculaSalario(), 000);
+    }
+
+
 }
